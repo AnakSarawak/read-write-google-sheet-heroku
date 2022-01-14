70b38b23c12daa0b602ae99417c12d80ffeb800c
AnakSarawak <96113022+AnakSarawak@users.noreply.github.com>
2022-01-14 10:56:40 +0800

    Update google.py

--- google.py
+++ google.py
@@ -1,24 +1,26 @@
+
+from datetime import datetime
+import time
 import os
-import json
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 
-# based on https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
-# read that file for how to generate the creds and how to use gspread to read and write to the spreadsheet
+from apscheduler.schedulers.background import BackgroundScheduler
+from pytz import HOUR, timezone
 
-# use creds to create a client to interact with the Google Drive API
-scopes = ['https://spreadsheets.google.com/feeds']
-json_creds = os.getenv("GOOGLE_SHEETS_CREDS_JSON")
 
-creds_dict = json.loads(json_creds)
-creds_dict["private_key"] = creds_dict["private_key"].replace("\\\\n", "\n")
-creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
-client = gspread.authorize(creds)
+def train_model():
+    print('Hello! The time is: %s' % datetime.now())
 
-# Find a workbook by url
-spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1RcQuetbKVYRRf0GhGZQi38okY8gT1cPUs6l3RM94yQo/edit#gid=704459328")
-sheet = spreadsheet.sheet1
 
-# Extract and print all of the values
-rows = sheet.get_all_records()
-print(rows)
+if __name__ == '__main__':
+    scheduler = BackgroundScheduler(timezone="Asia/Kuala_Lumpur")
+    scheduler.add_job(train_model, 'interval', minutes=1)
+    scheduler.start()
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    try:
+        # This is here to simulate application activity (which keeps the main thread alive).
+        while True:
+            time.sleep(2)
+    except (KeyboardInterrupt, SystemExit):
+        # Not strictly necessary if daemonic mode is enabled but should be done if possible
+        scheduler.shutdown()
